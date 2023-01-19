f0e02166aa8e5e99292b353f627c03a1317bc2ec
benbraide <benbraide@yahoo.com>
2023-01-19 09:09:23 +0100

    Enabled entry point; Feature update

--- src/inlinejs-collections.ts
+++ src/inlinejs-collections.ts
@@ -1,24 +1,3 @@
-import { GetGlobal, WaitForGlobal } from '@benbraide/inlinejs';
+import { InlineJSCollections } from './entry';
 
-import { CartCollectionConcept } from './concept/cart';
-import { FavoritesCollectionConcept } from './concept/favorites';
-
-import { CartDirectiveHandlerCompact } from './directive/cart';
-import { FavoritesDirectiveHandlerCompact } from './directive/favorites';
-
-import { CartMagicHandlerCompact } from './magic/cart';
-import { FavoritesMagicHandlerCompact } from './magic/favorites';
-
-WaitForGlobal().then(() => {
-    const cartCollection = new CartCollectionConcept(GetGlobal().CreateComponent(document.createElement('template')));
-    const favCollection = new FavoritesCollectionConcept(GetGlobal().CreateComponent(document.createElement('template')));
-    
-    GetGlobal().SetConcept(cartCollection.GetName(), cartCollection);
-    GetGlobal().SetConcept(favCollection.GetName(), favCollection);
-
-    CartDirectiveHandlerCompact();
-    FavoritesDirectiveHandlerCompact();
-
-    CartMagicHandlerCompact();
-    FavoritesMagicHandlerCompact();
-});
+InlineJSCollections();
